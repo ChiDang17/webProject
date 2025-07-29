069334cb2f1f5261883d68490329c8395818cd37
Chi <81267793+ChiDang17@users.noreply.github.com>
2025-07-29 14:04:42 -0700

    added specific product page; when you click on featured product on homepage it takes to you that page

--- frontend/src/app/specific_product/page.tsx
+++ frontend/src/app/specific_product/page.tsx
@@ -1,3 +1,25 @@
+import { FooterBar } from "@/components/FooterBar";
+import { HeaderBar } from "@/components/HeaderBar";
+
 export default function specific_product() {
-    return <div>Specific Product Page</div>;
+    return (
+        <div style={{ backgroundColor: "white", minHeight: "100vh"}}>
+            <div id="header-bar">
+                <HeaderBar></HeaderBar>
+            </div>
+            <div id="product-info" style={{ minHeight: "100vh", top: 100, display: "flex", alignItems: "center", justifyContent: "center", gap: "250px"}}>
+                <div className="product-picture" style={{ height: 550, width: 550, position: "relative"}}>
+                    <img src="/images/example-product.jpg"/>
+                </div>
+                <div id="product-text">
+                    <p style={{ color: "black", fontFamily: "Georgia, serif", fontSize: 25, left: "25px" }}>Example Product Name</p>
+                    <p style={{ color: "black", fontFamily: "Georgia, serif", fontSize: 20, left: "25px" }}>Price</p>
+                    <p style={{ color: "black", fontFamily: "Georgia, serif", fontSize: 15, left: "25px" }}>Product Description</p>
+                </div>
+            </div>
+            <div id="footer" style={{ minHeight: "50px" }}>
+                <FooterBar></FooterBar>
+            </div>
+        </div>
+    )
 }
--- frontend/src/components/FeaturedProducts.tsx
+++ frontend/src/components/FeaturedProducts.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ProductCard } from "./ProductCard";
+import Link from "next/link";
 
 
 export const FeaturedProducts = () => {
@@ -7,16 +8,24 @@ export const FeaturedProducts = () => {
         <div style={{ position: "relative", top: 25}}>
             <p style={{ color: "black", fontFamily: "Georgia, serif", fontSize: 25, position: "relative", left: "25px" }}>Featured Products:</p>
                 <div className="card-row" style={{ padding: "30px", textAlign: "justify", display: "inline-block"}}>
-                    <ProductCard></ProductCard>
+                    <Link href={`/specific_product`}>
+                        <ProductCard></ProductCard>
+                    </Link>
                 </div>
                 <div className="card-row" style={{ padding: "30px", textAlign: "justify", display: "inline-block"}}>
-                    <ProductCard></ProductCard>
+                    <Link href={`/specific_product`}>
+                        <ProductCard></ProductCard>
+                    </Link>
                 </div>
                 <div className="card-row" style={{ padding: "30px", textAlign: "justify", display: "inline-block"}}>
-                    <ProductCard></ProductCard>
+                    <Link href={`/specific_product`}>
+                        <ProductCard></ProductCard>
+                    </Link>
                 </div>
                 <div className="card-row" style={{ padding: "30px", textAlign: "justify", display: "inline-block"}}>
-                    <ProductCard></ProductCard>
+                    <Link href={`/specific_product`}>
+                        <ProductCard></ProductCard>
+                    </Link>
                 </div>
         </div>
     )
