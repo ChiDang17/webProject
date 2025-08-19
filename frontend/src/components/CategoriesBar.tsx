19a1a9fdd413c6ff7d4e18b47ca5000484fca9af
Chi <81267793+ChiDang17@users.noreply.github.com>
2025-08-19 14:52:18 -0700

    fixed categories bar a little

--- frontend/src/components/CategoriesBar.tsx
+++ frontend/src/components/CategoriesBar.tsx
@@ -19,7 +19,7 @@ export const CategoriesBar = () => {
     }, []);
 
     return (
-        <div>
+        <div className="flex no-scrollbar space-x-md">
             <Link 
                 href={`/list_of_products?category=all`} 
                 className='category-bar-buttons'
